a144469ff6271c0019d01f2bc4c6d1df10758eda
ichksk <good.ksuke.001@gmail.com>
2025-03-21 02:04:28 +0900

    fix some

--- src/components/leaveRoomButton.tsx
+++ src/components/leaveRoomButton.tsx
@@ -14,7 +14,7 @@ export const LeaveRoomButton = () => {
             router.push("/")
           })
       }}
-      className="mt-12 px-6 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition-colors duration-200"
+      className="mt-12 px-6 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition-colors duration-200 cursor-pointer"
     >
       退出する
     </button>
