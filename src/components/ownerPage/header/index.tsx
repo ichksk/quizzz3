7a4f5c0eb9a54bfae5046627293f819fea1f0054
ichksk <good.ksuke.001@gmail.com>
2025-03-21 02:05:48 +0900

    fix some

--- src/components/ownerPage/header/index.tsx
+++ src/components/ownerPage/header/index.tsx
@@ -1,15 +1,8 @@
-import { getRoomData } from "@/server/actions"
 import { RoomCodeField } from "./roomCode"
 import { RoomStatusBadge } from "./roomStatusBadge"
 import { RoomForOwner } from "@/types/schemas"
 
-export const Header = async () => {
-  const { room } = await getRoomData()
-
-  if (!room) {
-    return null
-  }
-
+export const Header = ({ room }: { room: RoomForOwner }) => {
   return (
     <div className="flex items-center justify-between">
       <RoomCodeField room={room as RoomForOwner} />
--- src/components/ownerPage/index.tsx
+++ src/components/ownerPage/index.tsx
@@ -16,7 +16,7 @@ export const OwnerPage = async () => {
 
   return (
     <div className="flex flex-col container mx-auto py-8 space-y-6">
-      <Header />
+      <Header room={room as RoomForOwner} />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <ParticipantsList roomCode={room.roomCode} currentUserId={participant.id} />
         <QuizController room={room as RoomForOwner} />
