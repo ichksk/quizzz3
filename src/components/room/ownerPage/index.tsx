c4cdea555d291320225ce14a01cd8dd0e1d31e28
ichksk <good.ksuke.001@gmail.com>
2025-03-21 04:32:49 +0900

    fix some

--- src/components/room/ownerPage/index.tsx
+++ src/components/room/ownerPage/index.tsx
@@ -7,7 +7,7 @@ import { Participant, RoomForOwner } from "@/types/schemas"
 export const OwnerPage = ({ room, participant }: { room: RoomForOwner, participant: Participant }) => {
 
   return (
-    <div className="flex flex-col container mx-auto py-8 space-y-6">
+    <div className="flex flex-col container mx-auto py-8 px-4 space-y-6">
       <Header room={room as RoomForOwner} />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <ParticipantsList roomCode={room.roomCode} currentUserId={participant.id} />
