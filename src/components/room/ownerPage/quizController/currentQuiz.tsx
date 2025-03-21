43c744387fe925dcbe13367d3128628b68df89f5
ichksk <good.ksuke.001@gmail.com>
2025-03-21 17:01:26 +0900

    fix some

--- src/components/room/ownerPage/quizController/currentQuiz.tsx
+++ src/components/room/ownerPage/quizController/currentQuiz.tsx
@@ -41,7 +41,7 @@ export const CurrentQuiz = () => {
           第{room.currentOrder + 1}問 / 全{quizzes.length}問
         </p>
         <button
-          className="flex items-center px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-200 shadow-md"
+          className="flex items-center px-5 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-200 shadow-md cursor-pointer"
           onClick={proceedQuiz}
         >
           {renderIcon()}
