b1aa5989593c3200a603ecbf68e6c8520b5886ba
ichksk <good.ksuke.001@gmail.com>
2025-03-22 22:45:59 +0900

    コンテナのanimationを実装

---
+++ src/app/(container)/layout.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex justify-center items-center">
+      <AnimatePresence>
+        <motion.div
+          layout
+          transition={{ layout: { duration: 0.4, type: "spring" } }}
+          className="bg-white/80 backdrop-blur-sm rounded-2xl shadow-lg p-8 sm:p-10"
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+}
