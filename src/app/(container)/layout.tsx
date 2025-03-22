8a11dc1f568ee9318b0292cc99d7b6b0eeb37849
ichksk <good.ksuke.001@gmail.com>
2025-03-22 23:58:12 +0900

    fff

--- src/app/(container)/layout.tsx
+++ src/app/(container)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <AnimatePresence>
         <motion.div
           layout
-          transition={{ layout: { duration: 0.4 } }}
+          transition={{ layout: { duration: 0.3 } }}
           className="bg-white/80 backdrop-blur-sm rounded-2xl shadow-lg p-8 sm:p-10"
         >
           {children}
