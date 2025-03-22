076dd7913f57f19b526bd43409f90ae756ef9351
ichksk <good.ksuke.001@gmail.com>
2025-03-22 22:08:51 +0900

    container

--- src/app/(container)/template.tsx
+++ src/app/(container)/template.tsx
@@ -1,19 +1,25 @@
 "use client"
 
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { ReactNode } from 'react';
 
 export default function Template({ children }: { children: ReactNode }) {
+
   return (
-    <div className="flex justify-center items-center">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-        className="bg-white/80 backdrop-blur-sm rounded-2xl shadow-lg p-8 sm:p-10"
-      >
-        {children}
-      </motion.div>
-    </div>
+    <AnimatePresence mode='wait'>
+      <div className="flex justify-center items-center">
+        <motion.div
+          className="bg-white/80 backdrop-blur-sm rounded-2xl shadow-lg p-8 sm:p-10"
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      </div>
+    </AnimatePresence>
   );
 }
