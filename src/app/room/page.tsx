2232b0e9e9e7bcd75159deab98327113bc6d623e
ichksk <good.ksuke.001@gmail.com>
2025-03-23 02:46:03 +0900

    fix some

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -139,13 +139,9 @@ export default function RoomPage() {
 
   if (!room || !participant) return null;
 
-  return (
-    <div className="min-h-[100dvh] relative">
-      {participant.isOwner ? (
-        <OwnerPage />
-      ) : (
-        <ParticipantPage />
-      )}
-    </div>
-  );
+  return participant.isOwner ? (
+    <OwnerPage />
+  ) : (
+    <ParticipantPage />
+  )
 }
