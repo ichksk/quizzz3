d678d06e5a63fbe6c73393beb857b675678eead1
ichksk <good.ksuke.001@gmail.com>
2025-03-23 01:01:06 +0900

    fb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,15 +8,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD96HQnJ0oev2gkenbfqswqz2J15E-XC5M",
-  authDomain: "quizzz-2c331.firebaseapp.com",
-  projectId: "quizzz-2c331",
-  storageBucket: "quizzz-2c331.firebasestorage.app",
-  messagingSenderId: "190620294047",
-  appId: "1:190620294047:web:b6f65e20b456047aa13baa",
-  measurementId: "G-NYDWLKKNM3"
-};
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG!);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
